b3237d2cb0c19b3d67b8f3385f36db826f616e3b
Craig Roberts <craig.roberts11@outlook.com>
2024-03-26 21:03:41 +1030

    Added text box and http client to send list of case files to server for downloading

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CourtSystem.Data;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Windows;
 
 namespace CourtSystem;
@@ -13,5 +15,22 @@ public partial class MainWindow : Window {
         serviceCollection.AddWpfBlazorWebView();
         serviceCollection.AddBlazorWebViewDeveloperTools();
         Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+
+    private void HandleNewCourtListClick(object sender, RoutedEventArgs e) {
+        popup.IsOpen = true;
+    }
+
+    private void HandleOpenPreviousCourtListClick(object sender, RoutedEventArgs e) {
+
+    }
+
+    private async void save_btn_Click(object sender, RoutedEventArgs e) {
+        var client = new HttpClient();
+        var response = await client.PostAsJsonAsync("https://localhost:7150/test", txtBox.Text);
+    }
+
+    private void close_btn_Click(object sender, RoutedEventArgs e) {
+        popup.IsOpen = false;
     }
 }
