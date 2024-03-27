49a82a956635b706f1c586f8d5c223cc073edcd0
Craig Roberts <craig.roberts11@outlook.com>
2024-03-28 07:05:53 +1030

    Separted home and courtlist page. Home page handles loading new and previous courtlists

---
+++ CourtListPage.razor.cs
@@ -0,0 +1,46 @@
+﻿using CourtSystem.Data;
+using CourtSystem.Models.UI;
+
+namespace CourtSystem;
+public partial class CourtListPage {
+    private CourtList CourtList { get; set; } = default!;
+    public Defendant? ActiveDefendant { get; set; }
+    private string? _error { get; set; }
+    private readonly ApplicationDbContext _context = new();
+
+    protected override void OnInitialized() {
+        var output = CourtListParser.ReadCaseFiles("SeedData.json");
+        if (output is not null) {
+            CourtList = output;
+            CourtList.GenerateInformations();
+
+            ActiveDefendant = CourtList.Defendants.FirstOrDefault();
+            if (ActiveDefendant is not null) {
+                ActiveDefendant.ActiveCaseFile = ActiveDefendant?.CaseFiles.FirstOrDefault();
+            }
+        }
+        else {
+            _error = "Error reading file";
+        }
+
+    }
+
+    private void ActivateDefendant(Defendant defendant) {
+        ActiveDefendant = defendant;
+        if (ActiveDefendant.ActiveCaseFile is null) {
+            ActiveDefendant.ActiveCaseFile = ActiveDefendant.CaseFiles.First();
+        }
+    }
+
+    private string IsSelected(Defendant defendant) {
+        if (ActiveDefendant?.Id == defendant.Id) {
+            return "!bg-sky-700";
+        }
+
+        return "hover:bg-gray-500";
+    }
+
+    private void SaveCourtList() {
+
+    }
+}
--- Home.razor.cs
+++ Home.razor.cs
@@ -1,46 +1,75 @@
 ﻿using CourtSystem.Data;
+using CourtSystem.Helpers;
 using CourtSystem.Models;
+using CourtSystem.Models.UI;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace CourtSystem;
+
 public partial class Home {
-    private CourtList CourtList { get; set; } = default!;
-    public Defendant? ActiveDefendant { get; set; }
-    private string? _error { get; set; }
-    private readonly ApplicationDbContext _context = new();
-
-    protected override void OnInitialized() {
-        var output = CourtListParser.ReadCaseFiles("SeedData.json");
-        if (output is not null) {
-            CourtList = output;
-            CourtList.GenerateInformations();
-
-            ActiveDefendant = CourtList.Defendants.FirstOrDefault();
-            if (ActiveDefendant is not null) {
-                ActiveDefendant.ActiveCaseFile = ActiveDefendant?.CaseFiles.FirstOrDefault();
-            }
-        }
-        else {
-            _error = "Error reading file";
-        }
-
-    }
-
-    private void ActivateDefendant(Defendant defendant) {
-        ActiveDefendant = defendant;
-        if (ActiveDefendant.ActiveCaseFile is null) {
-            ActiveDefendant.ActiveCaseFile = ActiveDefendant.CaseFiles.First();
+    public ElementReference? LoadNewCourtListDialog { get; set; }
+    public ElementReference? LoadPreviousCourtListDialog { get; set; }
+    public string CaseFileNumbers { get; set; } = string.Empty;
+    private string? _error;
+    private async Task OpenNewCourtListDialog() {
+        if (LoadNewCourtListDialog is not null) { 
+            _error = null;
+            await JSRuntime.InvokeVoidAsync("openDialog", LoadNewCourtListDialog);
         }
     }
 
-    private string IsSelected(Defendant defendant) {
-        if (ActiveDefendant?.Id == defendant.Id) {
-            return "!bg-sky-700";
+    private async Task OpenPreviousCourtListDialog() {
+        if (LoadNewCourtListDialog is not null) {
+            _error = null;
+            await JSRuntime.InvokeVoidAsync("openDialog", LoadPreviousCourtListDialog);
         }
-
-        return "hover:bg-gray-500";
     }
 
-    private void SaveCourtList() {
+    private async Task FetchCourtList() {
+        var client = new HttpClient();
+        var caseFileNumbers = CaseFileNumbers.Split("\n").Where(e => !string.IsNullOrWhiteSpace(e));
+        var response = await client.PostAsJsonAsync($"{AppConstants.ApiBaseUrl}/generate-case-files", caseFileNumbers);
 
+        if (!response.IsSuccessStatusCode) {
+            await JSRuntime.InvokeVoidAsync("alert", "Failed to fetch court list.");
+            return;
+        }
+
+        var caseFiles = await response.Content.ReadFromJsonAsync<List<CaseFile>>();
+        if (caseFiles is null) {
+            _error = "Failed to fetch court list.";
+            return;
+        }
+
+        var defendants = caseFiles.Select(e => e.Defendant).Distinct().ToList();
+        foreach (var defendant in defendants) {
+            defendant.CaseFiles = caseFiles.Where(e => e.Defendant.Id == defendant.Id).ToList();
+        }
+
+        var courtList = new CourtList {
+            Date = DateTime.Now,
+            Defendants = defendants
+        };
+
+        var dbContext = new ApplicationDbContext();
+
+        dbContext.CourtLists.Add(courtList);
+        dbContext.SaveChanges();
+
+        NavManager.NavigateTo("/court-list", true);
+    }
+
+    private async Task CloseLoadNewCourtListDialog() {
+        if (LoadNewCourtListDialog is not null) { 
+            await JSRuntime.InvokeVoidAsync("closeDialog", LoadNewCourtListDialog);
+        }
+    }
+    private async Task CloseLoadPreviousCourtListDialog() {
+        if (LoadNewCourtListDialog is not null) { 
+            await JSRuntime.InvokeVoidAsync("closeDialog", LoadPreviousCourtListDialog);
+        }
     }
 }
